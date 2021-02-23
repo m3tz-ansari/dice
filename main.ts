c135b9d8a538d987d03b695bd08e13e3670feadb
m3tz-ansari <79511029+m3tz-ansari@users.noreply.github.com>
2021-02-23 13:15:47 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(1, 6))
+    basic.showNumber(2)
+    basic.showNumber(3)
+    basic.showNumber(4)
+    basic.showNumber(5)
+    basic.showNumber(6)
+})
+basic.forever(function () {
+	
+})
